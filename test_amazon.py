60ca665fd86da7b47a7771df50b0739b9fdc5bf1
TGuyot <guyot.thomas97@gmail.com>
2022-04-29 09:50:12 +0200

    fixed typo

--- test_amazon.py
+++ test_amazon.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from pageobjects import homepage
+from time import sleep
 
 def test_amazon1():
     driver = webdriver.Chrome()
     driver.get("https://amazon.fr/")
     driver.maximize_window()
+
     hmpage = homepage.Homepage(driver)
     hmpage.closeCookie()
     hmpage.open_all_menu()
     hmpage.open_book_category()
     hmpage.open_all_books()
-    driver.quit()
+
+    #driver.quit()
